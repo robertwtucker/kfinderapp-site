1f6b629e5d87c28c882dea38611f0037c0a49618
Robert Tucker <robertwtucker@gmail.com>
2024-11-05 13:57:59 -0500

    refactor: abstract header and footer layouts

---
+++ components/Footer.tsx
@@ -0,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Robert Tucker
+ * SPDX-License-Identifier: MIT
+ */
+
+import Link from './Link'
+
+export default function Component() {
+  return (
+    <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
+      <p className="text-muted-foreground text-xs">© 2024 Robert Tucker. All rights reserved.</p>
+      <nav className="flex gap-4 sm:ml-auto sm:gap-6">
+        <Link className="text-xs underline-offset-4 hover:underline" href="/legal/disclaimer">
+          Disclaimer
+        </Link>
+        <Link className="text-xs underline-offset-4 hover:underline" href="/legal/terms">
+          Terms of Service
+        </Link>
+        <Link className="text-xs underline-offset-4 hover:underline" href="/legal/privacy">
+          Privacy
+        </Link>
+      </nav>
+    </footer>
+  )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Robert Tucker
+ * SPDX-License-Identifier: MIT
+ */
+
+import { Smartphone } from 'lucide-react'
+
+import Link from './Link'
+
+export default function Component() {
+  return (
+    <header className="flex h-14 items-center px-4 lg:px-6">
+      <Link className="flex items-center justify-center" href="#">
+        <Smartphone className="mr-2 size-6" />
+        <span className="font-bold">KFinder</span>
+      </Link>
+      <nav className="ml-auto flex gap-4 sm:gap-6">
+        <Link className="text-sm font-medium underline-offset-4 hover:underline" href="#features">
+          Features
+        </Link>
+        <Link className="text-sm font-medium underline-offset-4 hover:underline" href="#download">
+          Download
+        </Link>
+        <Link className="text-sm font-medium underline-offset-4 hover:underline" href="#contact">
+          Contact
+        </Link>
+      </nav>
+    </header>
+  )
+}
